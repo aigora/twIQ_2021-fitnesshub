ce964a25b212fcf85a8bbca49376f159b27ef222
SergioRodrigu <80398598+SergioRodrigu@users.noreply.github.com>
2021-04-16 17:06:46 +0200

    Añadido una opcion de funcionabilidad
    Añadido la opcion de dar de alta del menú

--- Code/Menu.c
+++ Code/Menu.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <locale.h>
+struct socio {
+	char nom[100];
+	int nDNI;
+	char lDNI;
+	int tipo;
+	
+};
+
+
 int main ()
 {
 	setlocale(LC_ALL, "spanish");
@@ -10,11 +19,47 @@ int main ()
 	printf("3.- Modificar datos\n");
 	printf("4.- Ver datos\n");
 	printf("0.- Salir\n");
-	printf("Opción introducida: ");
+	Menu();
+}
+int Menu(){
+	int opcion;
+	printf("Opción introducida:\n");
 	scanf ("%d", &opcion);
+	int num,num2,num3;
+	struct socio socio;
 		switch(opcion){	
 		case 1:
-			// Realizar lo que tenga que hacer la llamada funcion de dar de alta
+			printf("Bienvenido, usted ha seleccionado 'dar de alta'\n");
+			printf("¿desea continuar? (En caso afirmativo pulse 1, en caso negativo pulse 2)\n");
+			scanf ("%d", &num);
+			if (num==1){
+				printf ("Para poder darle de alta por favor introduzca su nombre completo: ");
+				scanf ("%s", socio.nom);
+				fflush(stdin);
+				num2=2; 
+				while (num2 == 2){
+					printf ("Hola %s introduzca su DNI: ", socio.nom);
+					scanf("%d %c", &socio.nDNI, &socio.lDNI);
+					printf ("%d %c ¿es este su DNI? (En caso afirmativo pulse 1, en caso negativo pulse 2)\n", socio.nDNI, socio.lDNI);
+					scanf("%d", &num2);
+					system("cls");	
+					}	
+						printf ("Qué tipo de socio desea ser:\n");
+						printf("Tarifa normal(Pulse 1)\n");
+						printf("Tarifa VIP(Pulse 2)\n");
+						scanf("%d", &socio.tipo);
+							if (socio.tipo == 1){
+								printf("Usted ha elegido la opción de tarifa normal\n");
+								printf("Bienvenido %s (%d%c) al Gimnasio UPM ahora a ponerse en forma", socio.nom, socio.nDNI, socio.lDNI);
+						   }else if (socio.tipo==2){
+								printf("Usted ha elegido la opción de tarifa VIP\n");
+								printf("Bienvenido %s (%d%c) al Gimnasio UPM ahora a ponerse en forma", socio.nom, socio.nDNI, socio.lDNI);
+					} 	
+						
+			} else if(num==2){
+				system("cls");
+				return main();
+			}
 			return 0;
 		case 2:
 			// Realizar lo que tenga que hacer la llamada funcion de dar de baja
@@ -23,7 +68,7 @@ int main ()
 			// Realizar lo que tenga que hacer la llamada funcion de modificar datos
 			return 0;
 		case 4:
-		// Realizar lo que tenga que hacer la llamada funcion de ver datos
+			// Realizar lo que tenga que hacer la llamada funcion de ver datos
 			return 0;
 		case 0:
 			// Realizar lo que tenga que hacer la llamada salir
@@ -31,7 +76,6 @@ int main ()
 		default:
 			//Opcion no dada
 			return 0;
-	
+	}
 	}
 
-}
